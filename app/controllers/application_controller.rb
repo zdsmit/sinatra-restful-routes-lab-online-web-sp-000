0eb8644c641e159dd4346bbbfa508760d5a10d91
Zach  Smith <zdsmith001@gmail.com>
2019-12-04 18:32:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,13 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  patch "/recipes/:id" do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
 end
