1cf26446a6029e7e4ef32b4b529cecb768850bce
Zach  Smith <zdsmith001@gmail.com>
2019-12-04 18:20:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,31 @@ class ApplicationController < Sinatra::Base
 
   post "/recipes" do
     @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
   end
 
-  get "/recipes/:name" do
-    @recipe = Recipe.find_by(params[:name])
+  get "/recipes" do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get "/recipes/:id" do
+    @recipe = Recipe.find_by_id(params[:id])
     erb :show
+  end
+
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch "/recipes/:id" do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingedients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
   end
 
 end
