53a2d9f80acfe8e0f99dcfe72b4d2600a9cf822f
Zach  Smith <zdsmith001@gmail.com>
2019-12-04 18:49:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
     @recipes = Recipe.all
     erb :index
   end
-  
+
   get "/recipes/:id" do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
